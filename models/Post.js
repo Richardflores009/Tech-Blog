5d2a59ade9a9ca60921a54f2e6d2684471474e5e
Richy Rich <richardflores009@gmail.com>
2020-10-29 17:39:14 -0700

    models created

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,36 @@ const sequelize = require('../config/config')
 
 class Post extends Model {}
 
-Post.init()
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
 
 module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -3,4 +3,24 @@ const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+module.exports = {Comment, Post, User}
