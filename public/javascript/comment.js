ec111dce284a2f122e643959d25b96189d84c9f0
Richy Rich <richardflores009@gmail.com>
2020-11-01 15:01:47 -0700

    added some js

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,5 +1,26 @@
 async function commentCreate(event) {
     event.preventDefault();
 
-    const comment_text = document
-}
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim()
+
+    const postId = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ]
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                postId,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        document.location.reload();
+    }
+};
+
+document.querySelector('#new-comment-form').addEventListener('submit', commentCreate)
